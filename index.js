7643865fde5e12672ed2051468d49ccbd825ca06
Carol A. Nikolic <carolnikolic@gmail.com>
2023-11-10 17:07:02 +0100

    feat: add http request methods.

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+
+app.get("/", (req, res) => {
+    res.send("<h1>Home Page</h1>");
+});
+
+app.post("/register", (req, res) => {
+    res.sendStatus(201);
+});
+
+app.put("/user/Carol", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.patch("/user/Carol", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.delete("/user/Carol", (req, res) => {
+    res.sendStatus(200);
+});
+
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 });
